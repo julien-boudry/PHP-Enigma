8fd6389e2d6d3cc481fb15027cbe75cb6a3dd410
Julien Boudry <julien.boudry@gmail.com>
2025-07-28 17:51:04 +0200

    Refactor tests to use Pest framework and remove legacy PHPUnit tests

---
+++ tests/Feature/OfficialExamplesTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+test('example simple', function () {
+    $r = true;
+
+    try {
+        ob_start();
+        include __DIR__.'/../../ExampleSimple.php';
+        ob_end_clean();
+    } catch (Exception $e) {
+        $r = false;
+        throw $e;
+    }
+
+    self::assertTrue($r);
+});
+
+test('example html', function () {
+    $r = true;
+
+    try {
+        ob_start();
+        include __DIR__.'/../../ExampleHtml.php';
+        ob_end_clean();
+    } catch (Exception $e) {
+        $r = false;
+        throw $e;
+    }
+
+    self::assertTrue($r);
+});
